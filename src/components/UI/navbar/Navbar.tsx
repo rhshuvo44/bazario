9c0268ab17bd96cadc6ee8ef0f0aed7fe7ab43ac
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-12-29 15:28:54 +0600

    navbar responsive

--- src/components/UI/navbar/Navbar.tsx
+++ src/components/UI/navbar/Navbar.tsx
@@ -17,53 +17,43 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-[#232F3E] text-white">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-[#232F3E] rounded-box z-[1] mt-3 w-auto p-2 shadow"
-          >
-            {navLinks.map((link) => (
-              <li key={link.path} className="px-1">
-                <Link
-                  href={link.path}
-                  className={`${pathname === link.path ? "active" : ""}`}
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <ul className="menu menu-horizontal px-1 bg-[#232F3E] hidden md:block w-52">
+      <div className="navbar-start w-full">
+        <ul className="menu menu-horizontal px-1 bg-[#232F3E] hidden md:block w-64">
           <li>
             <details className="dropdown-hover">
               <summary className="uppercase hover:text-white focus-visible:text-white active:text-white z-[99999999]">
                 Shop Categories
               </summary>
-              <ul className="p-2 bg-[#232F3E] rounded-sm z-[9999]">
+              <ul className="p-2 bg-[#232F3E] rounded-sm z-[9999] w-64">
                 {renderMenu(categoryNavLinks)}
               </ul>
             </details>
           </li>
         </ul>
+
+        <div className="md:hidden flex items-center justify-center w-full">
+          <div className="flex">
+            <div className="flex items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5">
+              <svg
+                viewBox="0 0 20 20"
+                aria-hidden="true"
+                className="pointer-events-none absolute w-5 fill-gray-500 transition"
+              >
+                <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              className="w-full pl-2 text-black font-semibold outline-0"
+              placeholder="Products Search"
+            />
+            <input
+              type="button"
+              value="Search"
+              className="bg-[#FEBD69] p-2 rounded-tr-lg rounded-br-lg text-white font-semibold  transition-colors cursor-pointer"
+            />
+          </div>
+        </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 bg-[#232F3E]">
--- src/components/UI/navbar/TopMenu.tsx
+++ src/components/UI/navbar/TopMenu.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { categoryNavLinks } from "@/constant";
+import renderMenu from "@/utils/CategoryMenu";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -98,11 +100,94 @@ const TopMenu = () => {
       ),
     },
   ];
+  const mobileNavLinks = [
+    {
+      path: "/compare-product",
+      label: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+          />
+        </svg>
+      ),
+    },
+    {
+      path: "/wishlist",
+      label: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+          />
+        </svg>
+      ),
+    },
+    {
+      path: "/login",
+      label: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+          />
+        </svg>
+      ),
+    },
+    {
+      path: "/cart",
+      label: (
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+            />
+          </svg>
+          <span className=" badge-lg indicator-item">8</span>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <div className="bg-[#131921] text-white w-full">
       <div className="flex justify-between flex-col flex-wrap md:flex-row px-6 py-2">
-        <p>Free Shipping Over $100 & Free Returns</p>
+        <p className="hidden md:block">
+          Free Shipping Over $100 & Free Returns
+        </p>
         <p>
           Hotline: <a href="tel:+1645252545">+1645252545</a>
         </p>
@@ -111,27 +196,36 @@ const TopMenu = () => {
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+            <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="inline-block h-5 w-5 stroke-current">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-[#131921] rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-[#131921]  z-[1]  w-60"
             >
-              {navLinks.map((link) => (
-                <li key={link.path} className="px-1">
+              {renderMenu(categoryNavLinks)}
+              
+            </ul>
+          </div>
+          <Link
+            href="/"
+            className="btn btn-ghost hover:bg-transparent text-[#FEBD69] text-lg md:text-3xl"
+          >
+            Bazario
+          </Link>
+          <div className="navbar-center flex lg:hidden">
+            <ul className="menu menu-sm menu-horizontal">
+              {mobileNavLinks.map((link) => (
+                <li key={link.path} className="">
                   <Link
                     href={link.path}
                     className={`${pathname === link.path ? "active" : ""}`}
@@ -142,9 +236,6 @@ const TopMenu = () => {
               ))}
             </ul>
           </div>
-          <Link href="/" className="btn btn-ghost hover:bg-transparent text-[#FEBD69] text-3xl">
-            Bazario
-          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 gap-2">
